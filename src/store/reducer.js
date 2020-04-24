75ecfcadefaf6552382aca0c843fe9319da5e3a6
Jerry <luongvidu@gmail.com>
2020-04-24 22:19:33 +1000

    Working on the totalPrice calculation

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,6 +10,13 @@ const initialState = {
     totalPrice: 4
 }
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 //We don't need break statement because we will return the case anyway,
 //so the code executions won't continue in this function.
 const reducer = ( state = initialState, action) => {
@@ -22,7 +29,8 @@ const reducer = ( state = initialState, action) => {
                     ...state.ingredients,
                     //Watch tutorial folder set dynamic property keys
                     [action.ingredientName]:state.ingredients[action.ingredientName] + 1
-                }
+                },
+                totalPrice : state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
                 
             };
         case actionTypes.REMOVE_INGREDIENT:
@@ -32,7 +40,8 @@ const reducer = ( state = initialState, action) => {
                     ...state.ingredients,
                     //Watch tutorial folder set dynamic property keys
                     [action.ingredientName]:state.ingredients[action.ingredientName] -1
-                }
+                },
+                totalPrice : state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
             };
         default:
             return state;
