6ac1aa88ee32a975879a280616d8d16db45987c9
Jerry <luongvidu@gmail.com>
2020-05-02 20:51:32 +1000

    Initializing ingredients in the burgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,20 +20,12 @@ class BurgerBuilder extends Component {
 
     state = {
         purchasing: false,
-        loading: false,
-        error: null
     }
 
-    // componentDidMount () {
-    //     //append .json to make it work
-    //     axios.get('https://react-my-bugger-7b8f8.firebaseio.com/ingredients.json')
-    //     .then(response => {
-    //         this.setState({ingredients: response.data});
-    //     })
-    //     .catch(error => {
-    //         this.setState({error:true});
-    //     });
-    // }
+    componentDidMount () {
+        //append .json to make it work
+        this.props.onInitIngredients();
+    }
     
     updatePurchaseState(ingredients) {
         const sum = Object.keys(ingredients)
@@ -76,7 +68,7 @@ class BurgerBuilder extends Component {
         let orderSummary = null;
         
 
-        let burger = this.state.error ? <p>Ingredients can't be loaded</p> : <Spinner />
+        let burger = this.props.error ? <p>Ingredients can't be loaded</p> : <Spinner />
         if (this.props.ings){
             burger = (
                 <>
@@ -97,9 +89,6 @@ class BurgerBuilder extends Component {
                             purchaseContinued = {this.purchaseContinueHandler}
                             price={this.props.price.toFixed(2)} />;
         }
-        if(this.state.loading) {
-            orderSummary = <Spinner />;
-        }
         return(
             <>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
@@ -114,14 +103,16 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        error: state.error
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         onIngredientAdded: (ingName) => dispatch(actionCreators.addIngredient(ingName)),
-        onIngredientRemoved: (ingName) => dispatch(actionCreators.removeIngredient(ingName))
+        onIngredientRemoved: (ingName) => dispatch(actionCreators.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(actionCreators.initIngredients())
     }
 }
 export default connect(mapStateToProps,mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios))
