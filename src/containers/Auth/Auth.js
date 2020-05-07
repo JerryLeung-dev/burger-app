5508e9f9a1835e5985ccb40cc291c6397ffb029e
Jerry <luongvidu@gmail.com>
2020-05-07 21:49:13 +1000

    Sharing the validation medthod

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -8,7 +8,7 @@ import classes from './Auth.module.css';
 import Spinner from '../../components/UI/Spinner/Spinner';
 
 import * as actions from '../../store/actions/index';
-import { updateObject } from '../../shared/utility';
+import { updateObject, checkValidity } from '../../shared/utility';
 
 
 class Auth extends Component {
@@ -53,7 +53,7 @@ class Auth extends Component {
         const updatedControls = updateObject(this.state.controls, {
             [controlName] : updateObject(this.state.controls[controlName], {
                 value: event.target.value,
-                valid: this.checkValidity( event.target.value, this.state.controls[controlName].validation),
+                valid: checkValidity( event.target.value, this.state.controls[controlName].validation),
                 touched: true
             })
         });       
@@ -66,31 +66,7 @@ class Auth extends Component {
         this.setState({controls: updatedControls, formIsValid: formIsValid});
     }
 
-    checkValidity(value, rules) {    
-        let isValid = true;
-        if (!rules) {
-            return true;
-        }
-        
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-        if(rules.isPassword){
-            const pattern = /(?=.{8,})/;
-            isValid = pattern.test(value);
-        }
-        return isValid;
-    }
+    
 
     submitHandler = (event) => {
         event.preventDefault();
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,29 @@ export const updateObject = (oldObject, updatedProperties) => {
         ...updatedProperties
     }
 }
+
+export const checkValidity = (value, rules) => {    
+    let isValid = true;
+    if (!rules) {
+        return true;
+    }
+    
+    if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.isEmail) {
+        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        isValid = pattern.test(value) && isValid
+    }
+
+    if (rules.isNumeric) {
+        const pattern = /^\d+$/;
+        isValid = pattern.test(value) && isValid
+    }
+    if(rules.isPassword){
+        const pattern = /(?=.{8,})/;
+        isValid = pattern.test(value);
+    }
+    return isValid;
+}
